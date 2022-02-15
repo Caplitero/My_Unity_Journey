8349f1c646354b86508dc5cb5a04876ce225f89e
Caplitero <98284271+Caplitero@users.noreply.github.com>
2022-02-15 20:12:30 +0200

    Update ButtonCode.cs

--- ButtonCode.cs
+++ ButtonCode.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This is just a test for the buttons 
+// buttons can be added from GameObjects->UI
+// After we created one , we add the script into it
+// to make the button write to log on click , we go to OnClick() funtion in the right 
+// press "+" and add the button , then sellect the funtion
+
 public class TextBox : MonoBehaviour
 {
     public void OnButtonPress()
