a99d945026765dcc24d66f9ead6c2b9593f134dd
Caplitero <98284271+Caplitero@users.noreply.github.com>
2022-02-15 20:10:11 +0200

    Add files via upload

---
+++ ButtonCode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBox : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        Debug.Log("hello");
+    }
+}
